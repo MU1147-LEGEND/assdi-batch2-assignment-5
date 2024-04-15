bcc2d1885833c80d76ed1de70ff1bc044fa0a464
Mohammad Ullah <ullahmohammad314@gmail.com>
2024-04-15 22:49:12 +0600

    Refactor menu toggle function, update services section heading styles, and add case studies section to index.html

--- script.js
+++ script.js
@@ -1,11 +1,36 @@
-function mouseHover(){
-    document.querySelector("#site-icon").setAttribute("fill", "red");
+function mouseHover() {
+  document.querySelector("#site-icon").setAttribute("fill", "red");
 }
-function mouseHoverOut(){
-    document.querySelector("#site-icon").setAttribute("fill", "black");
+function mouseHoverOut() {
+  document.querySelector("#site-icon").setAttribute("fill", "black");
 }
-function toggleMenu(e){
-    var menu = document.querySelector(".menus").children[1];
-    menu.classList.toggle("menu-expand-js");
-    e.firstElementChild.classList.toggle("xmark");
+function toggleMenu(e) {
+  var menu = document.querySelector(".menus").children[1];
+  menu.classList.toggle("menu-expand-js");
+  e.firstElementChild.classList.toggle("xmark");
 }
+function toggleAccordion(e) {
+  var inputElem = e.previousElementSibling;
+  var parentElem = e.parentElement;
+  if (inputElem.checked) {
+    inputElem.checked = false;
+    parentElem.style.backgroundColor = "#f2f2f2";
+  } else {
+    inputElem.checked = true;
+    parentElem.style.backgroundColor = "#b9ff66";
+  }
+}
+function accordionItemBackgroundChange() {
+  let owpItem = document.querySelectorAll(".owp-accordion-item");
+  owpItem.forEach((item) => {
+    item.addEventListener("click", function () {
+      let inputElem = document.querySelectorAll("input[type='radio']");
+      inputElem.forEach((elem) => {
+        if (!elem.checked) {
+          elem.parentElement.style.backgroundColor = "#f2f2f2";
+        }
+      });
+    });
+  });
+}
+accordionItemBackgroundChange();
